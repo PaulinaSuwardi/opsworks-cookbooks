64ae4f57fdaf8a65f5095e8158c385fed03a5291
PaulinaSuwardi <takara_gbu@yahoo.com>
2015-06-24 16:46:25 +0800

    Enable dependencies on windows

--- 7-zip/metadata.rb
+++ 7-zip/metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.3"
 supports         "windows"
 
-#depends          "windows", ">= 1.2.2"
+depends          "windows", ">= 1.2.2"
