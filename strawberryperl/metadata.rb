2277bc057ffb2eb549720ace311b813712172675
PaulinaSuwardi <takara_gbu@yahoo.com>
2015-06-24 18:34:55 +0800

    remove strawberryperl/metadata.rb README.md dependency

--- strawberryperl/metadata.rb
+++ strawberryperl/metadata.rb
@@ -1,6 +1,5 @@
 name             "strawberryperl5_16_3_1"
 description      "Installs/Configures the strawberry perl file archiver"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "15.16.3.1"
 supports         "windows"
 
