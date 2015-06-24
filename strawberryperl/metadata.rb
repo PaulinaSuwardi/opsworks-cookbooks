945fbf89a5e80d4d4f26465d736e651862c620f7
PaulinaSuwardi <takara_gbu@yahoo.com>
2015-06-24 18:46:51 +0800

    Modify strawberryperl/metadata.rb version

--- strawberryperl/metadata.rb
+++ strawberryperl/metadata.rb
@@ -1,6 +1,6 @@
 name             "strawberryperl5_16_3_1"
 description      "Installs/Configures the strawberry perl file archiver"
-version          "15.16.3.1"
+version          "0.1.0"
 supports         "windows"
 
 depends          "windows", ">= 1.2.2"
