f3e70bf56dec9789da8e564dd56b26eab0333f7a
PaulinaSuwardi <takara_gbu@yahoo.com>
2015-06-26 15:22:57 +0800

    Create directory before download zip from S3

--- winzip/recipes/default.rb
+++ winzip/recipes/default.rb
@@ -1,6 +1,15 @@
 Chef::Log.info("******Winzip install actions******")
 
-Chef::Log.info("******Downloading replaced files from S3******")
+Chef::Log.info("******Winzip create download directory******")
+download_filepath = "#{default['builder']['download_folder']}\\#{default['builder']['download_filename']}"
+
+directory node['builder']['download_folder'] do
+  mode '0777'
+  action :create
+  not_if { ::File.directory?(default['builder']['download_folder'] ) }
+end
+
+Chef::Log.info("******Winzip downloading from S3******")
 
 chef_gem "aws-sdk" do
   compile_time false
@@ -17,15 +26,22 @@ ruby_block "download-object" do
 
     s3_client.get_object(bucket: node['winzip']['s3_bucket'],
                      key: node['winzip']['s3_key'],
-                     response_target: node['winzip']['zip_path'])
+                     response_target: download_filepath)
   end
   action :run
 end
 
-Chef::Log.info("******unzip to local folder path******")
+Chef::Log.info("******Winzip unzip to local folder path******")
 
 windows_zipfile node['winzip']['home'] do
-  source node['winzip']['zip_path']
+  source download_filepath
   action :unzip
   not_if { ::File.directory?(node['winzip']['home']) }
+end
+
+Chef::Log.info("******Winzip add to windows path******")
+
+# update path
+windows_path node['7-zip']['home'] do
+  action :add
 end
